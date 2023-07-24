d4ee4c0b206afca9e50a1f466ad27b33b9e3266c
visheshtgupta <vishesht.gupta@mutualmobile.com>
2023-07-24 12:24:49 +0530

    splash screen added

--- src/screens/SplashScreen/index.jsx
+++ src/screens/SplashScreen/index.jsx
@@ -1,12 +1,25 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {View, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-const SplashScreen = () => {
-  return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  );
+const SPLASH_TIME_OUT = 2000;
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+      navigation.navigate('Home');
+    }, SPLASH_TIME_OUT);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <View style={styles.flex} />;
 };
 
-export default SplashScreen;
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});
+
+export default Splash;
